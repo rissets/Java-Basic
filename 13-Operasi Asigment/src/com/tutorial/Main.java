63525eed34f6b7272a7825cccbe931bbfcc1d24f
danangharissetiawan <Mr.danangharissetiawan@gmail.com>
2020-02-07 00:58:53 +0700

    Eps 13 Belajar Operator Assigment

---
+++ 13-Operasi Asigment/src/com/tutorial/Main.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.tutorial;
+
+/**
+ *
+ * @author haris
+ */
+public class Main {
+    public static void main(String[] args){
+        // Operator Assigment berlaku bagi semua operator atirmatika
+        
+        // Operator assigment penjumlahan
+        int a = 10;
+        a += 5;
+        System.out.println("nilai a = "+ a);
+        
+        // Operator Assigment pengurangan
+        int b = 10;
+        b -= 5;
+        System.out.println("nilai b = " + b);
+        
+        // Operator Assigment perkalian
+        int c = 10;
+        c *= 5;
+        System.out.println("nilai c = " + c);
+        
+        // Operator Assigment Pembagian
+        int d = 10;
+        d /= 5;
+        System.out.println("nilai d = " + d);
+        
+        // Operator Assigment Modulus
+        int e = 10;
+        e %= 7;
+        System.out.println("nilai e = " + e);
+        
+    }
+}
